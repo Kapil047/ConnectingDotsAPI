12fccd7d6786ed6d02dbff6f3e625903e3b571cf
Kapil <176895515+Kapil047@users.noreply.github.com>
2026-06-17 12:04:20 +0530

    Delete Conecting dots directory

--- Conecting dots/Services/ErrorLoggingService/IErrorLoggingService.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace ConnectingDotsAPI.Services.ErrorLoggingService
-{
-    public interface IErrorLoggingService
-    {
-        Task LogError(string exception, string innerException, string pageUrl, int? customerId, int logLevelId = 10);
-    }
-}
